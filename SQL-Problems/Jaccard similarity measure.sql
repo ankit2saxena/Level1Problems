d10ef18fea164cae837cf5e1f00ef05da8bc3608
ankit2saxena <ankit2saxena@gmail.com>
2016-11-03 11:40:01 -0400

    SQL code for Jaccard similarity measure
    The Jaccard similarity measure J(A,B) of two sets A and B is the quantity:
    mod(A INTERSECTION B) / mod(A UNION B)
    
    Consider tables Book(BookNo, Title, Price) and Cites(BookNo, CitedBookNo).
    Refer to book.txt and Cites.txt for data.
    Consider now our Cites relation and consider a pair of books (b1, b2).
    Let C(b1) denote the set of books cited by book b1 and let C(b2) denote the set of books cited by book b2. 
    Then J(C(b1), C(b2)) gives a measure of how similar books b1 and b2 are in terms of the books they cite. 
    If J(C(b1), C(b2)) = 1 then books b1 and b2 cite the same books. If J(C(b1), C(b2)) = 0 then books b1 and b2 do not cite any common book.

---
+++ SQL-Problems/Jaccard similarity measure.sql
@@ -0,0 +1,52 @@
+﻿create table Book(
+BookNo INTEGER,
+Title VARCHAR(30),
+Price INTEGER,
+primary key(BookNo));
+
+create table Cites(
+BookNo INTEGER references Book(BookNo),
+CitedBookNo INTEGER references Book(BookNo),
+primary key(BookNo,CitedBookNo));
+
+create or replace function get_Jaccard_Count (bno1 integer, bno2 integer)
+returns FLOAT AS $JC$
+declare 
+JCI FLOAT; 
+JCU FLOAT;
+JC FLOAT;
+BEGIN
+select count(P.CitedBookNo) from 
+(SELECT C1.CitedBookNo from Cites C1 where C1.BookNo = bno1
+INTERSECT
+SELECT C2.CitedBookNo from Cites C2 where C2.BookNo = bno2) P INTO JCI;
+
+select count(Q.CitedBookNo) from 
+(SELECT C1.CitedBookNo from Cites C1 where C1.BookNo = bno1
+UNION
+SELECT C2.CitedBookNo from Cites C2 where C2.BookNo = bno2) Q INTO JCU;
+
+JC = JCI/JCU;
+
+return JC;
+END;
+$JC$ LANGUAGE plpgsql;
+
+
+create or replace function Jaccard(l float, u float)
+returns table (book1 integer, book2 integer) AS $$
+BEGIN
+RETURN QUERY
+select C1.BookNo as book1, C2.BookNo as book2
+from Cites C1, Cites C2
+where C1.BookNo <> C2.BookNo
+AND get_Jaccard_Count(C1.BookNo, C2.BookNo) >= l
+AND get_Jaccard_Count(C1.BookNo, C2.BookNo) <= u
+GROUP BY C1.BookNo, C2.BookNo
+order by C1.BookNo, C2.BookNo;
+END;
+$$ LANGUAGE plpgsql;
+
+select book1, book2 from Jaccard(0,1);
+select book1, book2 from Jaccard(0,0);
+select book1, book2 from Jaccard(1,1);
